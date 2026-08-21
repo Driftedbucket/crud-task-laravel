71dfe248624cab9f8667c5b4691bf8b406396ebe
Driftedbucket <ndinowix@gmail.com>
2026-08-21 09:05:28 +0200

    feat: generate automatic routes

--- task-crud/routes/web.php
+++ task-crud/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tasks',TaskController::class);
